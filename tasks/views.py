98f3634ef700320464a60d65f79177f6dcb97f7b
Pouria <pouriagheitouri@gmail.com>
2026-07-04 13:35:13 -0700

    add home template

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("🔥 OK WORKING")
+    return render(request, "tasks/home.html")
