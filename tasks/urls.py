d73f382f397b00993a80e0b814806e4568e41f67
Pouria <pouriagheitouri@gmail.com>
2026-07-04 14:49:57 -0700

    add task form and templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,3 +4,17 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add/', views.add_task, name='add_task'),
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add/', views.add_task, name='add_task'),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 def home(request):
-    return render(request, "tasks/home.html")
+    tasks = Task.objects.all()
+    return render(request, "tasks/home.html", {"tasks": tasks})
+
+
+def add_task(request):
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, "tasks/add_task.html", {"form": form})
