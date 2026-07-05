ca28e8b25e5c916a2dcc1df6f29f49e83569c521
Pouria <pouriagheitouri@gmail.com>
2026-07-05 02:10:20 -0700

    complete task manager CRUD

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_task, name='add_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('complete/<int:task_id>/', views.toggle_complete, name='toggle_complete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,18 @@ from .models import Task
 from .forms import TaskForm
 
 def home(request):
-    tasks = Task.objects.all()
+    filter_type = request.GET.get("filter")
+
+    if filter_type == "completed":
+        tasks = Task.objects.filter(completed=True)
+
+    elif filter_type == "pending":
+        tasks = Task.objects.filter(completed=False)
+
+    else:
+        tasks = Task.objects.all()
+
     return render(request, "tasks/home.html", {"tasks": tasks})
-
-
 def add_task(request):
     form = TaskForm()
 
@@ -21,3 +29,22 @@ def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('home')
+def toggle_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    task.completed = not task.completed
+    task.save()
+
+    return redirect('home')
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, "tasks/edit_task.html", {"form": form})
