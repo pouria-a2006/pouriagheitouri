88c72b4af97642085b9a136f43273ffd0af10152
Pouria <pouriagheitouri@gmail.com>
2026-07-05 01:53:10 -0700

    add delete task feature

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,18 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
     path('add/', views.add_task, name='add_task'),
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('add/', views.add_task, name='add_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -17,3 +17,7 @@ def add_task(request):
             return redirect('home')
 
     return render(request, "tasks/add_task.html", {"form": form})
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('home')
